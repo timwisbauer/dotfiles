6428fc4b7d6c0e6f68aed2bdd978e7083e8e519b
Tim Wisbauer <timwisbauer@Tims-MacBook-Pro.local>
2024-08-07 12:57:02 -0500

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dotfiles
+
+My dotfiles managed with chezmoi.  Quickstart guide for chezmoi here: https://www.chezmoi.io/quick-start/#start-using-chezmoi-on-your-current-machine
+
+## Getting started
+
+Install chezmoi
+```
+sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+```
---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+brew install iterm2 fzf stow zsh
+
+brew install --cask font-meslo-lg-nerd-font
+
+QUICKSTART_DIR="${HOME}/.zsh-quickstart-kit"
+REPO_URL="https://github.com/unixorn/zsh-quickstart-kit.git"
+
+# Check if the directory exists
+if [ ! -d "$QUICKSTART_DIR" ]; then
+    echo "Cloning zsh-quickstart-kit..."
+    git clone "$REPO_URL" "$QUICKSTART_DIR"
+    echo "Cloned zsh-quickstart-kit into $QUICKSTART_DIR"
+else
+    echo "zsh-quickstart-kit already exists in $QUICKSTART_DIR"
+fi
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
