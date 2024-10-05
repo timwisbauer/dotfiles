63cc7b8ebb23d8dada0149b0308794884bc93702
Tim Wisbauer <tim.wisbauer@gmail.com>
2024-10-04 20:24:48 -0500

    include neo-tree

--- dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ dot_config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,35 +2,36 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-    filtered_items = {
-      visible = true,
-      show_hidden_count = true,
-      hide_dotfiles = false,
-      hide_gitignored = false,
-      hide_by_name = {
-        '.git',
-        '.DS_Store',
-        'thumbs.db',
-      },
-      never_show = {},
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_by_name = {
+					".git",
+					".DS_Store",
+					"thumbs.db",
+				},
+				never_show = {},
+			},
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
